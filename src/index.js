bc38dcd10836b2d8b0199eedd7b0690333f0659d
Prattek <pratteks@adobe.com>
2025-11-25 14:16:40 +0530

    add cors

--- src/index.js
+++ src/index.js
@@ -3,29 +3,36 @@ export default {
     const apiUrl =
       "https://clientapi.gcs-web.com/data/68ee6c33-8db5-45c4-8216-3dc41f586d9b/quotes";
 
-    try {
-      const res = await fetch(apiUrl, {
-        method: "GET",
+    // Handle preflight OPTIONS request
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
         headers: {
-          "Accept": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Max-Age": "86400",
         },
       });
+    }
 
-      if (!res.ok) {
-        return new Response(
-          JSON.stringify({ error: `Upstream error: ${res.status}` }),
-          { status: 500 }
-        );
-      }
+    try {
+      const res = await fetch(apiUrl);
 
       const data = await res.json();
 
       return new Response(JSON.stringify(data), {
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",     
+          "Access-Control-Allow-Methods": "GET",  
+        },
       });
     } catch (err) {
       return new Response(JSON.stringify({ error: err.message }), {
         status: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
       });
     }
   },
